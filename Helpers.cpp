12fb8dec773c203df6ba023eff7e7b1b2fe3003e
Piotr Rogalski <progalski89@gmail.com>
2022-01-18 01:11:40 +0100

    further work on boolReference

--- Helpers.cpp
+++ Helpers.cpp
@@ -3,4 +3,17 @@
 boolReference::boolReference(uint8_t& vectorElement, uint8_t elementPosition) :
     vectorElement_ {vectorElement}, elementPosition_ {elementPosition} {};
 
-boolReference::operator bool() const { return vectorElement_ & (1 << (elementPosition_ % 8)); }
+boolReference::operator bool() const { return vectorElement_ & (1 << elementPosition_); }
+
+boolReference& boolReference::operator=(bool rhs) {
+    if (rhs) {
+        vectorElement_ |= 1 << elementPosition_;
+    } else {
+        vectorElement_ &= 0 << elementPosition_;
+    }
+    return *this;
+}
+
+boolReference& boolReference::operator=(const boolReference& rhs) { operator=(rhs.get()); }
+
+bool boolReference::get() const { return vectorElement_ & (1 << elementPosition_); }
--- Helpers.hpp
+++ Helpers.hpp
@@ -5,6 +5,9 @@ class boolReference {
 public:
     boolReference(uint8_t& vectorElement, uint8_t elementPosition);
     explicit operator bool() const;
+    boolReference& operator=(bool rhs);
+    boolReference& operator=(const boolReference& rhs);
+    [[nodiscard]] bool get() const;
 
 private:
     uint8_t& vectorElement_;
